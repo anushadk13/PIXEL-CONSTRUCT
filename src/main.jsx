5d293851afd783312341322fabb5ddb71d0d0b4e
anushadk13 <122234698+anushadk13@users.noreply.github.com>
2024-07-04 12:25:51 +0530

    up

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Footer from './Footer'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <Footer/>
+  </React.StrictMode>,
+)
