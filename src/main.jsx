6f30c1b8e38cee5ec743c1f534bb22f5b591da43
anushadk13 <122234698+anushadk13@users.noreply.github.com>
2024-07-05 07:55:21 +0530

    up

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import Video from './Video'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
+    <Video/>
   <Footer/>
   </React.StrictMode>,
 )
